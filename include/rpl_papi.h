3106770ef42870ff9ba1995588826f9fdae755f5
Rahul Jadhav <nyrahul@gmail.com>
2020-10-05 14:00:21 +0530

    CMake compilation for librplcore.a works

---
+++ include/rpl_papi.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2020 Authors of Tripple
+ *
+ * This file is subject to the terms and conditions. See the file LICENSE in
+ * the top level directory for more details.
+ */
+
+/**
+ * @ingroup     PAPI
+ * @{
+ *
+ * @file
+ * @brief       Tripple Platform API exported header
+ *
+ * @}
+ */
+
+#ifndef _RPL_PAPI_H_
+#define _RPL_PAPI_H_
+
+#include <rpl.h>
+
+#endif // _RPL_PAPI_H_
